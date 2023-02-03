89839460fbccbfabafe2877d329a01223f7dc17b
plex <plex@onion.localdomain>
2023-02-03 17:11:24 +0300

    special symbols

--- chapter_01/hello/hello.go
+++ chapter_01/hello/hello.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	s := "Hello, Go!"     // string or series of bytes
 	s1 := "head first go" // string or series of bytes
-	ch := 'A'             // Rune
+	ch := 'A'             // Rune example
+	ch1 := 'b'            // Rune example
 
 	a := math.Floor(2.75) // result of function
 
@@ -17,4 +18,11 @@ func main() {
 	fmt.Println(strings.Title(s1))
 	fmt.Println(a)
 	fmt.Println("Rune example: ", ch)
+	fmt.Println("Rune example: ", ch1)
+	fmt.Println()
+
+	fmt.Println("Hello, \nGo!")     // new line character
+	fmt.Println("Hello, \tGo\t!")   // a tab character
+	fmt.Println("Hello, \"a\" Go!") // double quotation mark
+	fmt.Println("Hello, \\ Go!")    // a back slash
 }
