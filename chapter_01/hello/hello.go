0e11257cd6315a3ed62f5c78900172045ec0ac60
plex <plex@onion.localdomain>
2023-02-03 17:00:59 +0300

    strings and rune example

--- chapter_01/hello/hello.go
+++ chapter_01/hello/hello.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main() {
-	s := "Hello, Go!"
-	s1 := "head first go"
-	a := math.Floor(2.75)
+	s := "Hello, Go!"     // string or series of bytes
+	s1 := "head first go" // string or series of bytes
+	ch := 'A'             // Rune
+
+	a := math.Floor(2.75) // result of function
 
 	fmt.Println(s)
 	fmt.Println(strings.Title(s1))
 	fmt.Println(a)
+	fmt.Println("Rune example: ", ch)
 }
