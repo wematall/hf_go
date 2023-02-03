6c5faa71154971703aad614c9f074baa8dda82db
plex <plex@onion.localdomain>
2023-02-03 16:52:51 +0300

    math Floor package

--- chapter_01/hello/hello.go
+++ chapter_01/hello/hello.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func main() {
 	s := "Hello, Go!"
 	s1 := "head first go"
+	a := math.Floor(2.75)
 
 	fmt.Println(s)
 	fmt.Println(strings.Title(s1))
+	fmt.Println(a)
 }
