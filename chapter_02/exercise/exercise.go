1bbff588442b1b00655385dfe40a8a48af8d5ea3
plex <plex@onion.localdomain>
2023-02-14 06:03:26 +0300

    exercise scope of variables

---
+++ chapter_02/exercise/exercise.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Some of the lines of code below will result in a compile error,
+// because they refer to a variable that is out of scope.
+// Cross or comment out the lines that have errors
+
+var a = "a"
+
+func main() {
+	a = "a"
+	b := "b"
+	if true {
+		c := "c"
+		if true {
+			d := "d"
+			fmt.Println(a)
+			fmt.Println(b)
+			fmt.Println(c)
+			fmt.Println(d)
+		}
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+		// fmt.Println(d)
+	}
+	fmt.Println(a)
+	fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
+}
