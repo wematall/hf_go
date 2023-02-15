28f4dfa6da3f28ae8d3af6cab0b636139d33729a
plex <plex@onion.localdomain>
2023-02-15 07:54:06 +0300

    full declaration

---
+++ chapter_02/variable_declaration/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// variable declaration examples
+
+func main() {
+	// full declaration
+	var a string = "hello"
+	var b string
+	b = "world"
+
+	fmt.Println(a, b)
+
+}
