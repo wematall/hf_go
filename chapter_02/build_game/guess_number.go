39294038dba10316f3df78ae706c6d48423a9879
plex <plex@onion.localdomain>
2023-02-18 04:59:40 +0300

    user input added

--- chapter_02/build_game/guess_number.go
+++ chapter_02/build_game/guess_number.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,8 +15,25 @@ func main() {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
+
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
 	fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Make a guess: ")
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
