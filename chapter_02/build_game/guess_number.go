2bb87eb84dbbf635a77c89e7511defade416288d
plex <plex@onion.localdomain>
2023-02-19 06:07:21 +0300

    game guess a number

--- chapter_02/build_game/guess_number.go
+++ chapter_02/build_game/guess_number.go
@@ -1,3 +1,4 @@
+// guess challenges players to guess a random number.
 package main
 
 import (
@@ -18,31 +19,42 @@ func main() {
 
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
-
 	reader := bufio.NewReader(os.Stdin)
+	success := false
 
-	fmt.Println("Make a guess: ")
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
 
-	input, err := reader.ReadString('\n')
+		fmt.Println("Make a guess: ")
 
-	if err != nil {
-		log.Fatal(err)
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+
+			fmt.Println("Your number is ", guess)
+			fmt.Println("Our number is ", target)
+			break
+		}
 	}
 
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was:", target)
 	}
 
-	if guess < target {
-		fmt.Println("Oops. Your guess was LOW.")
-	} else if guess > target {
-		fmt.Println("Oops. Your guess was HIGH.")
-	} else {
-		fmt.Println("You guess a number.")
-		fmt.Println("Your number is ", guess)
-		fmt.Println("Our number is ", target)
-	}
 }
