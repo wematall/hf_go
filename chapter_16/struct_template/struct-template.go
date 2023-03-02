2bd7b92f36c74db79c69ceb2932f2baa3f510dc3
plex <plex@onion.localdomain>
2023-03-02 04:15:15 +0300

    struct Part

---
+++ chapter_16/struct_template/struct-template.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func executeTemplate(text string, data interface{}) {
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+	check(err)
+}
+
+type Part struct {
+	Name  string
+	Count int
+}
+
+func main() {
+	templateText := "Name: {{.Name}}\nCount: {{.Count}}\n"
+	executeTemplate(templateText, Part{Name: "Fuses", Count: 5})
+	executeTemplate(templateText, Part{Name: "Cables", Count: 2})
+}
