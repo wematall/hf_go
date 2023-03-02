04908ae9a89281f44981be08ef0e672eeab0c083
plex <plex@onion.localdomain>
2023-03-02 06:03:31 +0300

    redirect added

--- chapter_16/guestbook_app/guestbook.go
+++ chapter_16/guestbook_app/guestbook.go
@@ -61,6 +61,7 @@ func createHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 	err = file.Close()
 	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 }
 
 type Guestbook struct {
