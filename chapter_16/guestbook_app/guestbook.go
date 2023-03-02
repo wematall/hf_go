1094dfa3ce2a77120e8cae5eb3a9042a4e20690d
plex <plex@onion.localdomain>
2023-03-02 04:50:41 +0300

    read content from file

--- chapter_16/guestbook_app/guestbook.go
+++ chapter_16/guestbook_app/guestbook.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
 
 func check(err error) {
 	if err != nil {
@@ -13,10 +33,10 @@ func check(err error) {
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	fmt.Printf("%#v\n", signatures)
 	html, err := template.ParseFiles("view.html")
 	err = html.Execute(writer, nil)
-	// placeholder := []byte("signature list goes here")
-	// _, err := writer.Write(placeholder)
 	check(err)
 }
 
